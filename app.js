cc165d3176053dc6f9bf5a432327bbb2689370e9
Your Full Name <you@example.com>
2026-02-13 12:22:40 +0530

    version updated

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    res.send('DevOps - Version 1 🚀');
   res.send('DevOps Live Project - Version 1 🚀');
 });
 
